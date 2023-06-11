550604b6c5373704ec5919cb224912bfe3c0fb1f
kristin <kristin0996@web.de>
2023-06-11 15:47:24 +0200

    Add Solution for Line Numbering

---
+++ src/lineNumbering/LineNumbering.java
@@ -0,0 +1,23 @@
+package lineNumbering;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LineNumbering {
+	public static void main(String[] args) {
+        List<String> myList = Arrays.asList("a", "b", "c");
+        List<String> endresult = LineNumbering.number(myList);
+        System.out.println(endresult);
+     	}
+	
+	
+    public static List<String> number(List<String> lines) {
+    	for(int i=0; i<lines.size(); i++) {	//Aufbau Schleife: startwert für schleife;prüfung für schleife;zählt hoch
+			String anfangswert = Integer.toString(i+1);
+    		lines.set(i, anfangswert + ": " + lines.get(i));
+    	}
+        return lines;
+    }
+}
+
+
