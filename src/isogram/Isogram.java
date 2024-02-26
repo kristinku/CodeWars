fc560d757e98626fbf3f10ff246519b4a3ca5d93
kristin <kristin0996@web.de>
2024-02-26 22:48:25 +0100

    Isogramm true or false

---
+++ src/isogram/Isogram.java
@@ -0,0 +1,27 @@
+//An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that 
+//determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. 
+//Ignore letter case. Example: (Input --> Output
+//isIsogram "Dermatoglyphics" = true
+
+package isogram;
+
+import java.util.Arrays;
+
+public class Isogram {
+	public static void main(String[] args) {
+		boolean trueOrFalse = Isogram.isIsogram("A");
+		System.out.println(trueOrFalse);
+		
+	}
+    public static boolean  isIsogram(String str) {
+    	char[] strAsChar = str.toLowerCase().toCharArray();
+    	Arrays.sort(strAsChar);
+    	for(int i = 1; i < strAsChar.length; i++) {
+    		if(strAsChar[i] == strAsChar[i-1]) {
+    			return false;
+    		}
+    	}
+		return true;	
+    } 
+
+}
