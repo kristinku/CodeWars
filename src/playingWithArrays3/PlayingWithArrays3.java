a8450c8a721de677626744397d76a2097b691b94
kristin <kristin0996@web.de>
2024-04-02 22:50:35 +0200

    Given an array/list [] of integers , Find all the LEADERS in the array

---
+++ src/playingWithArrays3/PlayingWithArrays3.java
@@ -0,0 +1,29 @@
+//Given an array/list [] of integers , Find all the LEADERS in the array. An element is leader if it is greater than The Sum all the elements to its right side.
+
+package playingWithArrays3;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayingWithArrays3 {
+	public static void main(String[] args) {
+		int[] numbers = {5, 10, 2, 6};
+		List<Integer>leaders = PlayingWithArrays3.arrayLeaders(numbers);
+		System.out.println(leaders);
+	}
+    public static List<Integer> arrayLeaders(int[] numbers){
+		List<Integer> leaders = new ArrayList<>();
+		for(int i = 0; i < numbers.length; i++) {
+			if(numbers[i] > PlayingWithArrays3.sum(numbers, i)) {
+				leaders.add(numbers[i]);
+			}
+		}
+        return leaders; 
+    }
+    public static int sum(int[] numbers, int i ){
+    	int sum = 0;
+    	for(int j = i+1; j < numbers.length; j++) {
+    		sum += numbers[j];
+    		} 
+    	return sum;
+    }
+}
