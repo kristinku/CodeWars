3aed9ef4e1fd92fc034396482ac841641c49e521
kristin <kristin0996@web.de>
2024-04-07 22:17:57 +0200

    do-while schleife

---
+++ src/whileHochzählen/DoWhile.java
@@ -0,0 +1,12 @@
+package whileHochzählen;
+
+public class DoWhile {
+	public static void main(String[] args) {
+		int x = 0;
+		do {
+            System.out.println(x); 
+            x++;
+			
+		} while(x<=10);
+	}
+}
