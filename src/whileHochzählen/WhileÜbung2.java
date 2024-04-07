dab73052f70e355a97d1ecfcafa8dbdeda03eb86
kristin <kristin0996@web.de>
2024-04-07 22:00:17 +0200

    While Übung2 hochzählen

---
+++ src/whileHochzählen/WhileÜbung2.java
@@ -0,0 +1,11 @@
+package whileHochzählen;
+
+public class WhileÜbung2 {
+	public static void main(String[] args) {
+		int zahl = 0;
+		while (zahl <= 10) {
+			System.out.println("Zahl ist "+ zahl);
+			zahl++;
+		}
+	}
+}
