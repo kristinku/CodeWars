994b1d5c1f89a6c9aac95d786da6e65d8624fef1
kristin <kristin0996@web.de>
2024-04-16 23:20:24 +0200

    compare the specified limit with the individual int from array

---
+++ src/smallEnough/SmallEnough.java
@@ -0,0 +1,23 @@
+//You will be given an array and a limit value. You must check that all values in the array are below or equal 
+//to the limit value. If they are, return true. Else, return false.
+//You can assume all values in the array are numbers.
+
+package smallEnough;
+
+public class SmallEnough {
+	public static void main(String[] args) {
+		int[] numbers = {  101, 45, 75, 105, 99, 105, 107 };
+		boolean check = SmallEnough.smallEnough(numbers, 107);
+		System.out.println(check);
+	}
+
+	public static boolean smallEnough(int[] a, int limit) {
+		for (int number : a) {	
+			if(number > limit) {
+				   return false; 	
+			   }
+		}
+		return true;
+	}
+}
+
