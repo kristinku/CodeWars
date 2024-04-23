ab5dc6db3f8953266c164b1663c157d7554f88fe
kristin <kristin0996@web.de>
2024-04-23 21:15:40 +0200

    Add Kata: current position

---
+++ src/moves/Move.java
@@ -0,0 +1,15 @@
+//In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+//Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+package moves;
+
+public class Move {
+	public static void main(String[] args) {
+		int endposition = Move.move(3, 6);
+		System.out.println(endposition);
+	}
+	
+    public static int move(int position, int roll) {
+    	return (roll*2)+position;
+    }
+}
